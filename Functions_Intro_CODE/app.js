c39cec7ee0b27565fa4142df1ee63df79f80078c
wassemsabri10 <wassem.sabri10@gmail.com>
2023-12-16 14:34:19 -0600

    JavaScript

---
+++ Functions_Intro_CODE/app.js
@@ -0,0 +1,45 @@
+function singSong() {
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
+}
+
+function greet(firstName, lastName) {
+  console.log(`Hey there, ${firstName} ${lastName[0]}.`);
+}
+
+function repeat(str, numTimes) {
+  let result = "";
+  for (let i = 0; i < numTimes; i++) {
+    result += str;
+  }
+  console.log(result);
+}
+
+function add(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    return false;
+  }
+  return x + y;
+}
+
+// DEFINE YOUR FUNCTION BELOW:
+function sumArray(nums) {
+  let total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+  }
+  return total;
+}
+
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
